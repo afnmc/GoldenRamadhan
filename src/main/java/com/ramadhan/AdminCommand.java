41563fed4d8ef78ce0b139843195a211a43d97e7
Afn <abidfnufail.24@gmail.com>
2026-02-17 19:01:58 +0700

    Update AdminCommand.java

--- src/main/java/com/ramadhan/AdminCommand.java
+++ src/main/java/com/ramadhan/AdminCommand.java
@@ -1,48 +1,24 @@
 package com.ramadhan;
 
-import org.bukkit.Sound;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
+import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 
 public class AdminCommand implements CommandExecutor {
     private final GoldenMoon plugin;
-
-    public AdminCommand(GoldenMoon plugin) {
-        this.plugin = plugin;
-    }
+    public AdminCommand(GoldenMoon plugin) { this.plugin = plugin; }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player player)) return true;
-
-        if (args.length > 0) {
-            // Command: /gm daily
-            if (args[0].equalsIgnoreCase("daily")) {
-                new DailyGUI(plugin).open(player);
-                return true;
-            }
-
-            // Command: /gm getsword (Khusus Admin)
-            // PASTIKAN kamu ketik: /gm getsword (bukan getsworld)
-            if (args[0].equalsIgnoreCase("getsword")) {
-                if (!player.isOp()) {
-                    player.sendMessage("§cYou don't have permission.");
-                    return true;
-                }
-                player.getInventory().addItem(plugin.getDailyManager().getSpecialBlade());
-                player.sendMessage("§a[Admin] §fGolden Crescent Blade given!");
-                player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_USE, 1, 1);
-                return true;
-            }
-            
-            // Command Reset (Buat test ulang): /gm reset
-            if (args[0].equalsIgnoreCase("reset") && player.isOp()) {
-                plugin.getDailyManager().setClaimedLevel(player.getUniqueId(), 0);
-                // Jangan reset unlocked biar gak repot nunggu besok, cuma reset status klaim
-                player.sendMessage("§aData klaim direset! Sekarang bisa klaim ulang via GUI.");
-                return true;
+    public boolean onCommand(CommandSender s, Command c, String l, String[] a) {
+        if (!(s instanceof Player p)) return true;
+        if (a.length > 0) {
+            if (a[0].equalsIgnoreCase("daily")) {
+                new DailyGUI(plugin).open(p);
+            } else if (a[0].equalsIgnoreCase("getsword") && p.isOp()) {
+                p.getInventory().addItem(plugin.getDailyManager().getSpecialBlade());
+                p.sendMessage("§aPedang diberikan!");
+            } else if (a[0].equalsIgnoreCase("reset") && p.isOp()) {
+                plugin.getDailyManager().setClaimedLevel(p.getUniqueId(), 0);
+                p.sendMessage("§aData klaim direset!");
             }
         }
         return true;
