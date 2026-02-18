259249234eb2c4767f036e477752acacfa153e91
Afn <abidfnufail.24@gmail.com>
2026-02-18 12:40:31 +0700

    Update GoldenMoon.java

--- src/main/java/com/ramadhan/GoldenMoon.java
+++ src/main/java/com/ramadhan/GoldenMoon.java
@@ -5,27 +5,28 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class GoldenMoon extends JavaPlugin {
     public static NamespacedKey SWORD_KEY;
+    private DailyManager dailyManager;
 
     @Override
     public void onEnable() {
         SWORD_KEY = new NamespacedKey(this, "golden_crescent_blade");
         saveDefaultConfig();
+        
+        this.dailyManager = new DailyManager(this);
 
-        // Registrasi Listener
-        DailyGUI dailyGUI = new DailyGUI(this);
-        getServer().getPluginManager().registerEvents(dailyGUI, this);
+        // Registrasi Event
+        getServer().getPluginManager().registerEvents(new DailyGUI(this), this);
         getServer().getPluginManager().registerEvents(new SkillListener(this), this);
         
-        // Registrasi Command Admin
+        // Registrasi Command Admin (Buat ambil item)
         if (getCommand("goldenmoon") != null) {
             getCommand("goldenmoon").setExecutor(new AdminCommand(this));
         }
         
-        getLogger().info("§a[GoldenRamadhan] v16.8 Berhasil Load! Semua fitur aktif.");
+        getLogger().info("§6[GoldenRamadhan] §aPlugin v17.5 Aktif! Timezone: Abidjan.");
     }
 
-    @Override
-    public void onDisable() {
-        getLogger().info("§c[GoldenRamadhan] v16.8 Shutdown.");
+    public DailyManager getDailyManager() {
+        return dailyManager;
     }
 }
