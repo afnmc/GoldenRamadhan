e338e7eb30ee9c685a57bdcc5d7979339193e080
Afn <abidfnufail.24@gmail.com>
2026-02-18 12:07:37 +0700

    Update GoldenMoon.java

--- src/main/java/com/ramadhan/GoldenMoon.java
+++ src/main/java/com/ramadhan/GoldenMoon.java
@@ -5,7 +5,6 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GoldenMoon extends JavaPlugin {
-
     private static GoldenMoon instance;
     private DailyManager dailyManager;
     public static NamespacedKey SWORD_KEY;
@@ -15,7 +14,6 @@ public class GoldenMoon extends JavaPlugin {
         instance = this;
         SWORD_KEY = new NamespacedKey(this, "golden_crescent_blade");
         saveDefaultConfig();
-        
         this.dailyManager = new DailyManager(this);
 
         if (getCommand("goldenmoon") != null) {
@@ -26,16 +24,13 @@ public class GoldenMoon extends JavaPlugin {
         pm.registerEvents(dailyManager, this);
         pm.registerEvents(new SkillListener(this), this);
         pm.registerEvents(new DailyGUI(this), this);
-        pm.registerEvents(new QuestManager(this), this); // Pastikan ini ada
-
-        getLogger().info("Golden Moon v16 - Methods Restored!");
+        
+        getLogger().info("§aGolden Moon v16.2 - Sabit Runcing Aktif!");
     }
 
-    // Method yang dicari QuestManager dan MoonTask
     public String getMsg(String path) {
         String msg = getConfig().getString(path);
-        if (msg == null) return ChatColor.RED + "Missing: " + path;
-        return ChatColor.translateAlternateColorCodes('&', msg);
+        return (msg == null) ? "§cMissing: " + path : ChatColor.translateAlternateColorCodes('&', msg);
     }
 
     public DailyManager getDailyManager() { return dailyManager; }
