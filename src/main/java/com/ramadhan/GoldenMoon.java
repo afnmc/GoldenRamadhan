5898f8996e4aec702d256bc03a10e1412eff53a6
Afn <abidfnufail.24@gmail.com>
2026-02-18 11:38:10 +0700

    Update GoldenMoon.java

--- src/main/java/com/ramadhan/GoldenMoon.java
+++ src/main/java/com/ramadhan/GoldenMoon.java
@@ -16,33 +16,20 @@ public class GoldenMoon extends JavaPlugin {
         SWORD_KEY = new NamespacedKey(this, "golden_crescent_blade");
         saveDefaultConfig();
         
-        // Inisialisasi Manager
         this.dailyManager = new DailyManager(this);
 
-        // Register Command
         if (getCommand("goldenmoon") != null) {
             getCommand("goldenmoon").setExecutor(new AdminCommand(this));
         }
 
-        // Register Events
         var pm = getServer().getPluginManager();
         pm.registerEvents(dailyManager, this);
         pm.registerEvents(new SkillListener(this), this);
         pm.registerEvents(new DailyGUI(this), this);
 
-        getLogger().info("========================================");
-        getLogger().info("   Golden Ramadhan v14 - STABLE        ");
-        getLogger().info("   Sistem: Individual Progress         ");
-        getLogger().info("========================================");
+        getLogger().info("Golden Moon v15 - Full Build Moon Stabilized!");
     }
 
     public DailyManager getDailyManager() { return dailyManager; }
-
-    public String getMsg(String path) {
-        String msg = getConfig().getString(path);
-        if (msg == null) return ChatColor.RED + "Missing: " + path;
-        return ChatColor.translateAlternateColorCodes('&', msg);
-    }
-
     public static GoldenMoon getInstance() { return instance; }
 }
