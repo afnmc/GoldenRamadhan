6c255bd4e3b69d65eadf283350dd422b2a15ec4d
Afn <abidfnufail.24@gmail.com>
2026-02-18 12:26:11 +0700

    Update GoldenMoon.java

--- src/main/java/com/ramadhan/GoldenMoon.java
+++ src/main/java/com/ramadhan/GoldenMoon.java
@@ -1,38 +1,31 @@
 package com.ramadhan;
 
-import org.bukkit.ChatColor;
 import org.bukkit.NamespacedKey;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GoldenMoon extends JavaPlugin {
-    private static GoldenMoon instance;
-    private DailyManager dailyManager;
     public static NamespacedKey SWORD_KEY;
 
     @Override
     public void onEnable() {
-        instance = this;
         SWORD_KEY = new NamespacedKey(this, "golden_crescent_blade");
         saveDefaultConfig();
-        this.dailyManager = new DailyManager(this);
 
+        // Registrasi Listener
+        DailyGUI dailyGUI = new DailyGUI(this);
+        getServer().getPluginManager().registerEvents(dailyGUI, this);
+        getServer().getPluginManager().registerEvents(new SkillListener(this), this);
+        
+        // Registrasi Command Admin
         if (getCommand("goldenmoon") != null) {
             getCommand("goldenmoon").setExecutor(new AdminCommand(this));
         }
-
-        var pm = getServer().getPluginManager();
-        pm.registerEvents(dailyManager, this);
-        pm.registerEvents(new SkillListener(this), this);
-        pm.registerEvents(new DailyGUI(this), this);
         
-        getLogger().info("§aGolden Moon v16.2 - Sabit Runcing Aktif!");
+        getLogger().info("§a[GoldenRamadhan] v16.8 Berhasil Load! Semua fitur aktif.");
     }
 
-    public String getMsg(String path) {
-        String msg = getConfig().getString(path);
-        return (msg == null) ? "§cMissing: " + path : ChatColor.translateAlternateColorCodes('&', msg);
+    @Override
+    public void onDisable() {
+        getLogger().info("§c[GoldenRamadhan] v16.8 Shutdown.");
     }
-
-    public DailyManager getDailyManager() { return dailyManager; }
-    public static GoldenMoon getInstance() { return instance; }
 }
