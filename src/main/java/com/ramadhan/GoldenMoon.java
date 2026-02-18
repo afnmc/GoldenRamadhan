b5c2fc59d678baa948387b31c31b9b50123c4ae1
Afn <abidfnufail.24@gmail.com>
2026-02-18 07:26:28 +0700

    Update GoldenMoon.java

--- src/main/java/com/ramadhan/GoldenMoon.java
+++ src/main/java/com/ramadhan/GoldenMoon.java
@@ -1,62 +1,35 @@
 package com.ramadhan;
 
 import org.bukkit.ChatColor;
-import org.bukkit.NamespacedKey; // IMPORT INI
+import org.bukkit.NamespacedKey;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GoldenMoon extends JavaPlugin {
 
     private static GoldenMoon instance;
-    private DailyManager dailyManager;
-    
-    // KUNCI RAHASIA IDENTITAS PEDANG
     public static NamespacedKey SWORD_KEY;
 
     @Override
     public void onEnable() {
         instance = this;
-        
-        // Inisialisasi Key
         SWORD_KEY = new NamespacedKey(this, "golden_crescent_blade");
 
         saveDefaultConfig();
-        this.dailyManager = new DailyManager(this);
-        registerListeners();
-
+        
+        // Register Command
         if (getCommand("goldenmoon") != null) {
             getCommand("goldenmoon").setExecutor(new AdminCommand(this));
         }
 
-        new MoonTask(this).runTaskTimer(this, 0L, 20L);
-
-        getLogger().info("Golden Ramadhan v1.0 - ENABLED");
-    }
-
-    @Override
-    public void onDisable() {
-        getLogger().info("Golden Ramadhan v1.0 - DISABLED.");
-    }
-
-    private void registerListeners() {
+        // Register Listeners
         var pm = getServer().getPluginManager();
-        pm.registerEvents(dailyManager, this);
-        pm.registerEvents(new DailyGUI(this), this);
-        pm.registerEvents(new SkillListener(this), this); // Skill & Death Handler
-        pm.registerEvents(new QuestManager(this), this);
-        pm.registerEvents(new MoonTask(this), this);
+        pm.registerEvents(new SkillListener(this), this);
+
+        getLogger().info("========================================");
+        getLogger().info("   Golden Moon v13 - FINAL BUILD       ");
+        getLogger().info("   Status: Moonlight Ready!            ");
+        getLogger().info("========================================");
     }
 
-    public String getMsg(String path) {
-        String msg = getConfig().getString(path);
-        if (msg == null) return ChatColor.RED + "Config Missing: " + path;
-        return ChatColor.translateAlternateColorCodes('&', msg);
-    }
-
-    public DailyManager getDailyManager() {
-        return dailyManager;
-    }
-
-    public static GoldenMoon getInstance() {
-        return instance;
-    }
+    public static GoldenMoon getInstance() { return instance; }
 }
