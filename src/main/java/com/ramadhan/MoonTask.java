b53ad5b24f3ee1091364e31b7942f58fb45d13cf
Afn <abidfnufail.24@gmail.com>
2026-03-10 17:31:03 +0700

    Update MoonTask.java

--- src/main/java/com/ramadhan/MoonTask.java
+++ src/main/java/com/ramadhan/MoonTask.java
@@ -11,17 +11,24 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 public class MoonTask extends BukkitRunnable implements Listener {
     private final GoldenMoon plugin;
+    
     public MoonTask(GoldenMoon plugin) { this.plugin = plugin; }
+    
     @EventHandler
-    public void onJoin(PlayerJoinEvent e) { e.getPlayer().sendMessage(plugin.getMsg("welcome-message")); }
+    public void onJoin(PlayerJoinEvent e) { 
+        e.getPlayer().sendMessage(plugin.getMsg("welcome-message")); 
+    }
+    
     @Override
     public void run() {
         for (Player p : Bukkit.getOnlinePlayers()) {
             long time = p.getWorld().getTime();
-            if (time > 13000 && time < 23000) {
-                p.spawnParticle(Particle.DUST, p.getLocation().add(0, 3, 0), 5, new Particle.DustOptions(Color.fromRGB(255, 215, 0), 1.2f));
+            int start = plugin.getConfig().getInt("moon-effect-start", 13000);
+            int end = plugin.getConfig().getInt("moon-effect-end", 22500);
+            if (time >= start && time <= end) {
+                p.spawnParticle(Particle.DUST, p.getLocation().add(0, 3, 0), 5, 
+                    new Particle.DustOptions(Color.fromRGB(255, 215, 0), 1.2f));
             }
         }
     }
 }
-
